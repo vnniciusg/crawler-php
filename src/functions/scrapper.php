27a26316b5b43ad834abf5e55cfe7a594bb188dd
vnniciusg <90412694+vnniciusg@users.noreply.github.com>
2024-01-09 17:22:50 -0300

    refactor : change to browser kit

--- src/functions/scrapper.php
+++ src/functions/scrapper.php
@@ -2,53 +2,67 @@
 
 require "vendor/autoload.php";
 
-use Goutte\Client;
+use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\HttpClient\HttpClient;
 
 function crawlAmLeiloeiro($url)
 {
-    $client = new Client();
-
     $data = [];
+    $visitedUrls = [];
 
-    $selector = '.hover\:shadow-2xl.transition-all.ease-in.shadow-zinc-100.shadow-sm.dark\:shadow-zinc-800.dark\:bg-gradient-to-t.dark\:from-zinc-800.dark\:to-zinc-800.border-\[1px\].bg-white.dark\:border-zinc-800.mb-2.rounded-md.md\:m-0.m-2.group';
-    $crawler = $client->request('GET', $url);
+    $browser = new HttpBrowser(HttpClient::create());
 
-    $crawler = $crawler->filter($selector)->each(
-        function ($node) use ($client) {
-            $href = $node->filter("a")->attr("href");
-            if ($href) {
-                $selectorLote = '.hover\:shadow-2xl.transition-all.ease-in.shadow-zinc-100.shadow-sm.dark\:shadow-zinc-800.dark\:bg-gradient-to-t.dark\:from-zinc-800.dark\:to-zinc-800.border-\[1px\].bg-white.dark\:border-zinc-800.mb-2.rounded-md.md\:m-0.m-2.group';
-                $crawlerLote = $client->request("GET", $href);
-                $nodesLote = $crawlerLote->filter($selectorLote);
+    try {
+        $crawler = $browser->request('GET', $url);
 
-                if ($nodesLote->count() > 0) {
-                    $nodesLote->each(
-                        function ($nodeLote) use ($client) {
-                            $hrefItem = $nodeLote->filter("a")->attr("href");
-                            echo "URL lote: $hrefItem\n";
+        $crawler->filter('.hover\:shadow-2xl.transition-all.ease-in.shadow-zinc-100.shadow-sm.dark\:shadow-zinc-800.dark\:bg-gradient-to-t.dark\:from-zinc-800.dark\:to-zinc-800.border-\[1px\].bg-white.dark\:border-zinc-800.mb-2.rounded-md.md\:m-0.m-2.group')->each(
+            function ($node) use ($browser, &$data) {
+                $href = $node->filter("a")->link()->getUri();
 
-                            $crawlerItemInfo = $client->request("GET", $hrefItem);
+                if ($href) {
+                    $crawlerLote = $browser->request("GET", $href);
+                    $nodesLote = $crawlerLote->filter('.hover\:shadow-2xl.transition-all.ease-in.shadow-zinc-100.shadow-sm.dark\:shadow-zinc-800.dark\:bg-gradient-to-t.dark\:from-zinc-800.dark\:to-zinc-800.border-\[1px\].bg-white.dark\:border-zinc-800.mb-2.rounded-md.md\:m-0.m-2.group');
 
-                            $dataPrimeiroLote = $crawlerItemInfo->filter('td.py-3.px-4 span')->text();
-                            $dataSegundoLote = $crawlerItemInfo->filter('td.py-3.px-4 span')->nextAll()->text();
-                            $valor = $crawlerItemInfo->filter('tbody tr:contains("Valor da avaliação:") td.py-3.px-4')->text();
+                    $countNodesLote = $nodesLote->count();
 
-                            echo "Valor: $valor\n";
-                            echo "Data 1º lote: $dataPrimeiroLote\n";
-                            echo "Data 2º lote: $dataSegundoLote\n";
-                        }
-                    );
+                    if ($countNodesLote > 0) {
+                        $nodesLote->each(
+                            function ($nodeLote) use ($browser, &$data) {
+                                $hrefItem = $nodeLote->filter("a")->link()->getUri();
 
-                } else {
-                    echo "Nenhum lote encontrado\n";
+                                $crawlerItemInfo = $browser->request("GET", $hrefItem);
+                                echo "Coletando dados de $hrefItem\n";
+
+                                if ($crawlerItemInfo->filter('td.py-3.px-4 span')->count() > 0) {
+                                    $dataPrimeiroLote = $crawlerItemInfo->filter('td.py-3.px-4 span')->text();
+                                    $dataSegundoLote = $crawlerItemInfo->filter('td.py-3.px-4 span')->nextAll()->text();
+
+                                    $dataPrimeiroLote = str_replace("1º. LEILÃO:", "", $dataPrimeiroLote);
+                                    $dataSegundoLote = str_replace("2º. LEILÃO:", "", $dataSegundoLote);
+
+                                    $valor = $crawlerItemInfo->filter('tbody tr:contains("Valor da avaliação:") td.py-3.px-4')->text();
+
+                                    $data[] = [
+                                        "urlItem" => $hrefItem,
+                                        "valor" => $valor,
+                                        "dataPrimeiroLote" => $dataPrimeiroLote,
+                                        "dataSegundoLote" => $dataSegundoLote,
+                                    ];
+                                } else {
+                                    echo "Nenhum dado encontrado para $hrefItem\n";
+                                }
+                            }
+                        );
+                    } else {
+                        echo "Nenhum lote encontrado para $href\n";
+                    }
                 }
-                echo "----------------\n";
             }
-
-        }
-    )
-    ;
-
+        );
+    } catch (\Exception $e) {
+        echo "Erro: " . $e->getMessage() . "\n";
+        echo "Trace: " . $e->getTraceAsString() . "\n";
+    }
 
     return $data;
 }
